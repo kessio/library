3f7a9b1c39c11eab2216d292e236308a8e7e7f7a
kessio <64524758+kessio@users.noreply.github.com>
2022-08-01 19:02:28 +0300

    added to loca storage

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // Creating book objects and storing them in an Array
+const booksCard = document.querySelector('.display-books');
+
 const bookCollection = [];
 function createbook (event) {
   event.preventDefault();
@@ -9,27 +11,27 @@ function createbook (event) {
   bookCollection.push(book);
   document.querySelector('form').reset();
 }
-//console.log(bookCollection);
 
   let addBtn = document.getElementById('addBtn');
   addBtn.addEventListener('click', (event)  => {
     createbook(event);
     setData();
-    
   
   });
- 
+  
   function setData () {
-    let getBooks = localStorage.getItem('books');
-  if(getBooks === null){
-     books = [];
-  }else{
-   let oldData = JSON.parse(getBooks);
-   bookCollection.push(oldData);
- let newData = localStorage.setItem('books',JSON.stringify(bookCollection));
- return newData
- 
+    let newData = localStorage.setItem('books',JSON.stringify(bookCollection));
+    return newData;
+    
   }
 
+  function getStoredData() {
+    return JSON.parse(localStorage.getItem('books'));
+  
   }
- 
+  
+
+
+
+
+
