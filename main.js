5b5ba0f1e8e32e9d0c6dd8b3b31c18f9899d88e3
kessio <64524758+kessio@users.noreply.github.com>
2022-08-02 09:16:24 +0300

    Changes to localstorage

--- main.js
+++ main.js
@@ -1,44 +1,58 @@
-// Creating book objects and storing them in an Array
+
+const addBookBnt   = document.getElementById('addBtn');
 const booksCard = document.querySelector('.display-books');
 
-const bookCollection = [];
-function createbook (event) {
-  event.preventDefault();
-  const book = {
-    'title': document.getElementById('title').value,
-    'author': document.getElementById('author').value,
-  };
-  bookCollection.push(book);
-  document.querySelector('form').reset();
-}
-
-  let addBtn = document.getElementById('addBtn');
-  addBtn.addEventListener('click', (event)  => {
-    createbook(event);
-    setData();
-  
+ let getsrored = localStorage.getItem('books');
+ //console.log(getsrored);
+  let books;
+  if(localStorage.getItem('book') === ''){
+  books = [];
+  }else{
+  books = JSON.parse(getsrored);
+  //console.log(books);
+  }
+ 
+  addBookBnt.addEventListener('click', (e) => {
+    e.preventDefault();
+    books.push(addBook());
+   // console.log(books);
+   localStorage.setItem('books', JSON.stringify(books));
   });
-  
-  function setData () {
-    let newData = localStorage.setItem('books',JSON.stringify(bookCollection));
-    return newData;
-    
-  }
-
+   function addBook(){
+    const addedBook = {
+      'title': document.getElementById('title').value,
+      'author': document.getElementById('author').value,
+      'isbn': document.getElementById('isbn').value,
+    };
+    return addedBook;
+   }
   function getStoredData() {
-    return JSON.parse(localStorage.getItem('books'));
+    let storeddata = JSON.parse(localStorage.getItem('books'));
+   return storeddata;
   
   }
+  getStoredData();
   
 function renderBooks() { 
   let storedData = getStoredData(); 
   storedData.forEach((data) => { 
-  booksCard.innerHTML += `<p>${data.title}</p> 
-                          <button>Remove</button> 
-                          <hr>` 
+  booksCard.innerHTML += `
+  <div class="card-container">
+  <h4>${data.title}</h4> 
+  <p>${data.author}</p>
+  <button id="${data.isbn}" class="remove-book">Remove</button> 
+  <hr></div>`
+  
   }); 
 } 
 renderBooks(); 
+
+
+
+  
+
+
+
 
 
 
