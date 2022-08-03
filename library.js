08efd06e50a179102e57e6e33e0112fe8017a31b
kessio <64524758+kessio@users.noreply.github.com>
2022-08-03 16:10:43 +0300

    Changes on local storage

--- library.js
+++ library.js
@@ -1,25 +1,25 @@
-// Object constructor class
+/* eslint-disable max-classes-per-file */
 class Books {
-    constructor(title, author, id) {
-      this.title = title;
-      this.author = author;
-      this.id = id;
-  
-    }
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
 }
 
 // UI class
 class UserInterface {
-  // Loop books for display books 
+  // Loop books for display books
   static displayBooks() {
     const books = BookStorage.getLocalStorageBooks();
-    books.forEach((book) => (UserInterface.renderBooks(book)));
-
+    if (books != null) {
+      books.forEach((book) => (UserInterface.renderBooks(book)));
+    }
   }
 
   static renderBooks(book) {
-      const booksContainer = document.querySelector('.book-details');
-      booksContainer.innerHTML += `
+    const booksContainer = document.querySelector('.book-details');
+    booksContainer.innerHTML += `
       <tr>
       <td colspan="2">
           "${book.title}" by ${book.author}
@@ -28,12 +28,12 @@ class UserInterface {
           <button class="delete-btn" id="${book.id}">Remove</button>
       </td>
   </tr>
-      `
+      `;
   }
+
   static generateID() {
     return Math.floor(Math.random() * 100);
   }
-  
 }
 
 // LocalSTorage class
@@ -41,12 +41,12 @@ class BookStorage {
   // Get book from local storage
   static getLocalStorageBooks() {
     let bookks;
-    if(localStorage.getItem('books') === 'null') {
+    if (localStorage.getItem('books') === null) {
       bookks = [];
-    }else {
+    } else {
       bookks = JSON.parse(localStorage.getItem('books'));
-      return bookks;
     }
+    return bookks;
   }
 
   // Add book to local storage
@@ -57,48 +57,38 @@ class BookStorage {
   }
 
   // Remove book from local storage
-  static RemoveBookFromLocalStorage(button) {
-    // console.log(button.id)
-    const books = BookStorage.getLocalStorageBooks();
-    const filteredBooks = books.filter((book) => book.id !== button.id);
-    // console.log(filteredBooks)
-    // console.log(book.id)
-    localStorage.setItem('books', JSON.stringify(filteredBooks));
+  static RemoveBookFromLocalStorage(removeid) {
+    const booksList = BookStorage.getLocalStorageBooks();
+    const booksArray = booksList;
+    const newlist = booksArray.filter((item) => item.id != removeid);
+    localStorage.setItem('books', JSON.stringify(newlist));
   }
 }
 // Events: Render on page load
-document.addEventListener('DOMContentLoaded', UserInterface.displayBooks());
+UserInterface.displayBooks();
 
 // Events: Add books
 const form = document.getElementById('add-book');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
+form.addEventListener('submit', () => {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
-   const id = UserInterface.generateID();
-  //Instatiate class
-  const addedBooks = new Books (title, author, id);
-
-  // Render the books added
-  UserInterface.renderBooks(addedBooks);
+  const id = UserInterface.generateID();
+  // Instatiate class
+  const addedBooks = new Books(title, author, id);
 
   // Add book to local storage
   BookStorage.addBookToLocalStorage(addedBooks);
-})
+  // Render the books added
+  UserInterface.renderBooks(addedBooks);
+});
 // Events Remove books
-
 const removeBook = document.querySelectorAll('.delete-btn');
 removeBook.forEach((element) => {
   element.addEventListener('click', (e) => {
+    console.log(element);
     const button = e.target;
-    console.log(button)
+    const removeid = e.target.id;
+    BookStorage.RemoveBookFromLocalStorage(removeid);
     button.parentElement.parentElement.remove();
-
-    // Remove book from local storage
-    BookStorage.RemoveBookFromLocalStorage(button);
-
-  })
-})
-
-// Event: 
+  });
+});
