6757290513339e200b888b5a4f1339f746c600ef
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-08-03 09:45:56 -0700

    add library structure[input form] and some styles

---
+++ library.js
@@ -0,0 +1,19 @@
+// Object constructor class
+class Books {
+    constructor(title, author, isbn) {
+      this.title = title;
+      this.author = author;
+      this.isbn = isbn;
+  
+    }
+}
+
+// UI class
+
+// LocalSTorage class
+
+// Events: Add books
+
+// Events Remove books
+
+// Event: 
