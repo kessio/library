d101298403d8be7926b305cc53a3345ee24c4cd9
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-08-04 11:15:21 -0700

    fix duplicate ID error

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const container = document.querySelectorAll('.container');
 
 menuItems.forEach((navLink) => {
     navLink.addEventListener('click', (e) => {
-        // console.log(e.target.id);
-        const linkID = e.target.id;
-        // console.log(linkID)
+        // console.log(e.target.className);
+        const linkID = e.target.className;
 
         container.forEach((section) => {
-            // console.log(section.id)
+            console.log(section.id)
             section.classList.remove('active');
             if(section.id === linkID) {
                 section.classList.add('active');
